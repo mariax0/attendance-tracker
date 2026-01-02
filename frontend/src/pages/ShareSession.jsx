7c482d0f1a94ba82e407b4b415635f18321bca40
ale <tomescualexandramirela@gmail.com>
2026-01-02 22:21:33 +0200

    frontend(ui): fix Share Session page layout

--- frontend/src/pages/ShareSession.jsx
+++ frontend/src/pages/ShareSession.jsx
@@ -1,18 +1,18 @@
 import Navbar from "../components/navbar/NavBar"
-import "../styles/ShareSession.css"
+import styles from "../styles/ShareSession.module.css"
 
 function ShareSession() {
     return (
-        <div className="share-session">
+        <div className={styles.content}>
             <Navbar />
-            <div id="share-content">
-                <div id="headline">
+            <div className={styles.container}>
+                <div className={styles.heading}>
                     <h1>Share session</h1>
                     <p className="grey-text">Let participants confirm their attendance using event code or QR code</p>
                 </div>
-                <div id="event-details">
-                    <p id="event-code">022884</p>
-                    <div id="qr-code"></div>
+                <div className={styles.details}>
+                    <p className={styles.eventCode}>022884</p>
+                    <div className={styles.qrCode}></div>
                 </div>
             </div>
         </div>
