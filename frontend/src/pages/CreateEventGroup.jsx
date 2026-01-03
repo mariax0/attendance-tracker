c4d3d21e37a3eb9e2f5e0a610a00e7b05fbe7b87
ale <tomescualexandramirela@gmail.com>
2026-01-03 14:52:37 +0200

    frontend(create): change Create Event Group form

--- frontend/src/pages/CreateEventGroup.jsx
+++ frontend/src/pages/CreateEventGroup.jsx
@@ -1,24 +1,35 @@
 import Navbar from "../components/navbar/NavBar"
 import InputField from "../components/InputField"
 import Button from "../components/Button"
-import "../styles/ViewEventGroup.css"
+import styles from "../styles/CreateEventGroup.module.css"
 
 function CreateEventGroup() {
     return (
-        <div className="view-event-group">
+        <div className={styles.content}>
             <Navbar />
-            <div id="event-content">
-                <div id="headline">
+            <div className={styles.container}>
+                <div className={styles.heading}>
                     <h1>Create event group</h1>
                     <p className="grey-text">Define your event details and schedule</p>
                 </div>
-                    <div id="inputs">
+                    <form className={styles.inputs}>
                         <InputField label="Event Name" type="text" placeholder="Web Tech Lecture"/>
-                        <InputField label="Number of Sessions" type="text" placeholder="14"/>
-                        <InputField label="Event Day(s)" type="text" placeholder="[Select day(s)]"/>
-                        <InputField label="Event Time" type="text" placeholder="[HH:MM - HH:MM]"/>
+                        <InputField label="Number of Sessions" type="number" placeholder="14"/>
+                        <div className={styles.days}>
+                            <label className={`${styles.create} grey-text`}>Event Days</label>
+                            <div className={styles.checkboxes}>
+                                {["Mon", "Tue", "Wed", "Thu", "Fri"].map(day => (
+                                    <label key={day} className={styles.choice}>
+                                        <input className={styles.checkbox} type="checkbox"></input>
+                                        {day}
+                                    </label>
+                                ))}
+                            </div>
+                        </div>
+                        <InputField label="Event Start Time" type="time"/>
+                        <InputField label="Event End Time" type="time"/>
                         <Button text="Create"></Button>
-                    </div>
+                    </form>
             </div>
         </div>
     )
